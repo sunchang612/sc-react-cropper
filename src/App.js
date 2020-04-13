51fa1315e48b9b0d2d72f2a5ab1a7b9a47183b8c
sunchang1996 <sunchang612@gmail.com>
2020-04-13 15:34:41 +0800

    :sparkles: Drag or down the right to make it bigger

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ const initContentState = {
   selectArea: '',
   parentEl: '',
   isRefresh: false,
-  resizeArea: ''
+  resizeArea: '',
+  moveEl: '',
+  isMoveRefresh: false
 }
 
 function reducer(state, action) {
   switch(action.type) {
     case 'update':
+      console.log('action---------->', action)
       return {...state, ...action.data}
     case 'reset':
       return {...state, ...action.data}
@@ -22,13 +25,13 @@ function reducer(state, action) {
 
 function App() {
   const cropper = useRef()
+  const rightLine = useRef()
   const [state, dispatch] = useReducer(reducer, initContentState)
 
   // 移动开始
   const handleMouseStart = (e) => {
     // 当前的
     const curEl = cropper.current
-    console.log(cropper.current.offsetLeft, e.clientX)
     // 父级的
     const parentEl = cropper.current.parentElement
     // 选中区域
@@ -41,19 +44,7 @@ function App() {
       maxMoveY: parentEl.offsetHeight - curEl.offsetHeight
     }
    
-
     dispatch({type: 'update', data:{ curEl, parentEl, selectArea , isRefresh: true }})
-  
-    
-    // document.addEventListener(
-    //   'mousemove',
-    //   onMoveStart,
-    // )
-    // document.addEventListener(
-    //   'mouseup',
-    //   handleMoveCloses,
-    //   false
-    // )
   }
 
   useEffect(() => {
@@ -68,17 +59,24 @@ function App() {
         false
       )
     }
+    if (state.isMoveRefresh) {
+      document.addEventListener(
+        'mouseup',
+        handleMoveCloses,
+        false
+      )
+      document.addEventListener('mousemove', handleReset, false)
+    }
     return () => {
       document.removeEventListener('mousemove', onMoveStart, false)
       document.removeEventListener('mouseup', handleMoveCloses, false)
       document.removeEventListener('mousemove', handleReset, false)
     }
-  }, [state.isRefresh])
+  }, [state.isRefresh, state.isMoveRefresh])
 
   const onMoveStart = (e) => {
     const { curEl, parentEl, selectArea } = state
     if (!curEl || !parentEl || !selectArea) return
-
     // 移动中实时计算位置
     const moveLeft = e.clientX - selectArea.left
     const moveRight = e.clientY - selectArea.top
@@ -90,7 +88,7 @@ function App() {
 
   // 鼠标结束
   const handleMoveCloses = () => {
-    dispatch({type: 'reset', data:{ curEl: '', parentEl: '', selectArea: '' , isRefresh: false }})
+    dispatch({type: 'reset', data:{ curEl: '', parentEl: '', selectArea: '' , isRefresh: false, isMoveRefresh: false }})
   }
 
   // resize
@@ -107,43 +105,41 @@ function App() {
       left: parseInt(curEl.style.left, 10),
       top: parseInt(curEl.style.top, 10)
     }
-
-    dispatch({type: 'update', data:{ resizeArea, isRefresh: true }})
-    // console.log(resizeArea)
-    document.addEventListener('mousemove', handleReset(direction, e), false)
-    document.addEventListener('mouseup', handleMoveCloses, false)
+    dispatch({type: 'update', data:{ resizeArea, isMoveRefresh: true, direction, moveEl: e}})
   }
 
-  const handleReset = (direction, e) => {
-    const { resizeArea } = state
-    console.log('resizeArea ------>', resizeArea, e.clientX)
-    if (!resizeArea) return
+  const handleReset = (e) => {
+    const { resizeArea, direction, moveEl } = state
+    
+    const curEl = cropper.current
+
+    if (!resizeArea || !moveEl) return
+    // return
     // 获取父级元素
     const container = cropper.current.parentElement
     const parHeight = container.offsetHeight
     const parWidth = container.offsetWidth
     const parLeft = parseInt(container.style.left || 0, 10)
     const parRight = parseInt(container.style.top || 0, 10)
-    const curEl = cropper.current
+    // const curEl = cropper.current
     if (direction === 'r' || direction === 'b') {
       let moveLength
       if (direction === 'r') {
-        moveLength = resizeArea.width + e.clientX - resizeArea.posLeft
+        moveLength = resizeArea.width + parseInt(e.clientX, 10) - resizeArea.posLeft
       } else {
         moveLength = resizeArea.height + e.clientY - resizeArea.posTop
       }
-
-      console.log('moveLength---->', moveLength)
 
       if (parseInt(curEl.style.left, 0) + moveLength > parWidth || parseInt(curEl.style.top, 10) + moveLength > parHeight) {
         const w = parWidth - parseInt(curEl.style.left, 10)
         const h = parHeight - parseInt(curEl.style.top, 10)
         curEl.style.width = curEl.style.height = Math.min(w, h) + 'px'
       } else {
-        console.log('???')
         curEl.style.width = moveLength + 'px'
         curEl.style.height = moveLength + 'px'
       }
+    } else {
+
     }
   }
 
@@ -159,10 +155,10 @@ function App() {
         >
           <span className="dashed-line slide-dashed-x"></span>
           <span className="dashed-line slide-dashed-y"></span>
-          <span className="border-line slide-border-top"></span>
-          <span className="border-line slide-border-left"></span>
+          <span className="border-line slide-border-top" onMouseDown={(e) => handleResizeReady(e, 't')}></span>
+          <span className="border-line slide-border-left" onMouseDown={(e) => handleResizeReady(e, 'l')}></span>
           <span className="border-line slide-border-right" onMouseDown={(e) => handleResizeReady(e, 'r')}></span>
-          <span className="border-line slide-border-bom"></span>
+          <span className="border-line slide-border-bom" onMouseDown={(e) => handleResizeReady(e, 'b')}></span>
         </div>
       </div>
     </div>
